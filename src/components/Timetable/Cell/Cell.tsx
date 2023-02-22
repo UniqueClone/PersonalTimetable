dcbd19acbfed3667c84f54aea690b8811edfb147
Ryan Lynch <ryanlynch@microsoft.com>
2023-02-22 17:44:06 +0000

    Updating Cell

--- src/components/Timetable/Cell/Cell.tsx
+++ src/components/Timetable/Cell/Cell.tsx
@@ -1,9 +1,20 @@
 import { CellProps } from "./CellProps";
 import React from "react";
 import { Td } from "react-super-responsive-table";
+import { Text } from "@fluentui/react";
 import { getTimetableStyles } from "../TimetableStyles";
 
 export const Cell: React.FC<CellProps> = props => {
-    const styles = getTimetableStyles();
-    return <Td className={styles.cell}>{props.content}</Td>;
+    const styles = getTimetableStyles(undefined, props);
+    return (
+        <Td className={styles.cell}>
+            <Text variant="medium" className={styles.module}>
+                {props.module}
+            </Text>
+            <br />
+            <Text variant="small" className={styles.location}>
+                {props.location}
+            </Text>
+        </Td>
+    );
 };
--- src/components/Timetable/Cell/CellProps.ts
+++ src/components/Timetable/Cell/CellProps.ts
@@ -7,8 +7,38 @@ export interface CellProps {
      */
     className?: string;
 
+    color?: string;
+
+    /**
+     * The class length in hours.
+     * @example 1
+     * @example 2
+     */
+    length?: number;
+
+    /**
+     * The class location.
+     * @example "TH1"
+     * @example "JH6"
+     */
+    location?: string;
+
+    /**
+     * The Module Code.
+     * @example "CS601"
+     * @example "EP200"
+     */
+    module?: string;
+
+    /**
+     * The Module Name.
+     * @example "Introduction to Computer Science"
+     */
+    name?: string;
+
     /**
      * The text to display in the cell.
+     * Might be used for notes.
      */
     content?: string;
 }
